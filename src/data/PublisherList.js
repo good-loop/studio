83e13af39eb5504bed74f290cd1beb9c442abcd4
Daniel Winterstein <daniel@sodash.com>
2020-05-14 17:58:06 +0100

    rm

---
+++ src/data/PublisherList.js
@@ -0,0 +1,44 @@
+/**
+ * HACK hard-code a few high-profile publishers
+ * TODO replace this with ajax loading of data from the portal (with a cache)
+ */
+const publishers = [
+	{
+		name: 'www.buzzfeed.com',
+		branding: { logo: 'http://www.worksdesigngroup.com.php72-34.phx1-1.websitetestlink.com/wp-content/uploads/2017/05/buzzfeed-e1515438116988.png'},
+	},
+	{
+		name: 'winit.empireonline.com',
+		branding: { logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1l-KWl1ddiB47rGuU4qO6D_NPGGdDeB6WyM9vKLCPKSGC-stw&s'},
+	},
+	{
+		name: 'www.nme.com',
+		branding: { logo: 'https://s3.eu-central-1.amazonaws.com/centaur-wp/designweek/prod/content/uploads/2013/10/image001-1002x466.jpg'},
+	},
+	{
+		name: 'video.fyber.com',
+		branding: { logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw7YrflR2DhP9l0cUYUAP1juNZCKhV4VdvIvHW3X-9NHVmiTQE&s'}
+	},
+	{
+		name: 'ads.mopub.com',
+		branding: { logo: 'https://techcrunch.com/wp-content/uploads/2012/10/mopub-logo.png?w=550'}
+	},
+	{
+		name: 'wv.inner-active.mobi',
+		branding: { logo: 'https://1z1euk35x7oy36s8we4dr6lo-wpengine.netdna-ssl.com/wp-content/uploads/2015/10/inneractive_logo.jpg'}
+	},
+	{
+		name: 'www.rocketgeeks.com',
+		branding: { logo: 'https://rocketgeeks.com/wp-content/themes/unified/images/logo_footer.png'}
+	},
+	{
+		name: 'cheezburger.com',
+		branding: { logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWlrK7QO4j0ACfCKdspJdYMKWJF6QrbPCAniJ-6_iZiHaiwl2q&s'}
+	},
+	{
+		name: 'www.cracked.com',
+		branding: { logo: 'http://steve-lovelace.com/wordpress/wp-content/uploads/2013/09/cracked-logo-in-optima-font.png'}
+	}
+];
+
+export default publishers;
