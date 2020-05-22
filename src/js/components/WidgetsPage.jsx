4223919f039d45e6185cecd54ba90c0f5098bd84
Daniel Winterstein <daniel@good-loop.com>
2020-05-22 10:49:35 +0100

    sync

--- src/js/components/WidgetsPage.jsx
+++ src/js/components/WidgetsPage.jsx
@@ -52,6 +52,9 @@ const PropControlWidgets = ({filter}) => {
 	return (<><h4>PropControl Widgets</h4>
 		<PropControl prop='mybasictext' path={['widget','BasicTextPropControl']} 
 			label='Basic text input' help='Use this for text entry' />
+		
+		<PropControl type='radio' options={['Apples', 'Bananas', 'Pears']} prop='myradio' 
+			path={['widget','BasicTextPropControl']} help='Help would go here.' />
 	</>);
 };
 
