982a6301f9e9b587a3965bbaccd344232b3bc04f
Daniel Winterstein <daniel@good-loop.com>
2020-07-29 11:55:20 +0100

    .compact

--- src/js/components/LayoutWidgets.jsx
+++ src/js/components/LayoutWidgets.jsx
@@ -2,7 +2,7 @@
 import React, {useState} from 'react';
 import PropControl, { FormControl } from '../base/components/PropControl';
 import DataStore, {getValue} from '../base/plumbing/DataStore';
-import { Input, FormGroup, Label, Form, Card, CardTitle } from 'reactstrap';
+import { Input, FormGroup, Label, Form, Card, CardTitle, Button, Row, Col } from 'reactstrap';
 import Wizard, {WizardStage} from '../base/components/WizardProgressWidget';
 import WidgetExample from './WidgetExample';
 
@@ -25,6 +25,29 @@ const LayoutWidgets = () => {
 					All done :)
 				</WizardStage>
 			</Wizard>
+		</WidgetExample>
+
+		<WidgetExample name='Compact Form Layout' keywords='' >
+			<Row>
+				<Col>
+					<div className='compact'>
+						<PropControl prop='compact-1' label='My first compact label' value='Some text' />
+						<PropControl prop='compact-2' label='Another label with some help' help='How can we help you?' />
+						<PropControl prop='compact-3' label='Checkbox 1' type='checkbox' warning='Oh dear, blah blah' />
+						<PropControl prop='compact-4' label='Some free text' type='textarea' />
+						<Button>A Button</Button>
+					</div>
+				</Col>
+				<Col>
+					<div>
+						<PropControl prop='compact-1' label='My first normal label' />
+						<PropControl prop='compact-2' label='Another label with some help' help='How can we help you?' />
+						<PropControl prop='compact-3' label='Checkbox 1' type='checkbox' warning='Oh dear, blah blah' />
+						<PropControl prop='compact-4' label='Some free text' type='textarea' />
+						<Button>Finally, a button</Button>
+					</div>
+				</Col>
+			</Row>
 		</WidgetExample>
 
 	</Card>);
