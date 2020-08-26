08a311f08137e611a697c4cae41a6f4c97269092
Daniel Winterstein <daniel@good-loop.com>
2020-08-26 14:00:37 +0100

    dflt-works

--- src/js/components/SubCard.jsx
+++ src/js/components/SubCard.jsx
@@ -5,7 +5,7 @@ import {Card, CardTitle } from 'reactstrap';
 /**
  * A Card for use inside a larger Card
  */
-const SubCard = ({title,children}) => <Card body><CardTitle><h4>{title}></h4></CardTitle>{children</Card>;
+const SubCard = ({title,children}) => <Card body><CardTitle><h4>{title}</h4></CardTitle>{children}</Card>;
 
 export default SubCard;
  
