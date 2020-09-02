4eccf1cd8cd18b6c7b106ba99abd93f8469da9a4
Daniel Winterstein <daniel@good-loop.com>
2020-09-02 09:32:51 +0100

    substr

--- src/js/components/FunctionWidgets.jsx
+++ src/js/components/FunctionWidgets.jsx
@@ -6,6 +6,8 @@ import { Input, FormGroup, Label, Form, Card, CardTitle } from 'reactstrap';
 import Wizard, {WizardStage} from '../base/components/WizardProgressWidget';
 import WidgetExample from './WidgetExample';
 import SearchQuery from '../base/searchquery';
+import { substr } from '../base/utils/miscutils';
+import { assMatch } from '../base/utils/assert';
 
 const FunctionWidgets = () => {	
 	const lwpath = ['widget','FunctionWidgets'];
@@ -20,6 +22,11 @@ const FunctionWidgets = () => {
 			<PropControl path={lwpath} prop='q' label='Query' />
 			<pre>{JSON.stringify(sq)}</pre>
 			<pre>rm blue: {JSON.stringify(SearchQuery.remove(sq, "blue"))}</pre>
+		</WidgetExample>
+
+		<WidgetExample name='substr' keywords='substring string miscutils' >			
+			<pre>substr("foo bar", -2) = {assMatch(substr("foo bar", -2), "ar")}</pre>
+			<pre>substr("foo bar", 0, -2) = {assMatch(substr("foo bar", 0, -2), "foo b")}</pre>
 		</WidgetExample>
 
 	</Card>);
