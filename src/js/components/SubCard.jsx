91e87af69fbede25804f4c5c1b3dbdab21c68fac
Daniel Winterstein <daniel@good-loop.com>
2020-09-09 17:06:13 +0100

    sync

--- src/js/components/SubCard.jsx
+++ src/js/components/SubCard.jsx
@@ -5,7 +5,20 @@ import {Card, CardTitle } from 'reactstrap';
 /**
  * A Card for use inside a larger Card
  */
-const SubCard = ({title,children}) => <Card body><CardTitle><h4>{title}</h4></CardTitle>{children}</Card>;
+const SubCard = ({title,children}) => {
+	// TODO hide if nothing is visible
+	// let [show, setShow] = useState(false);
+	// // pass in setShow
+	// if (children) {
+	// 	// array of elements (or just one)? screen nulls
+	// 	if (children.filter) children = children.filter(x => !! x);
+	// 	children = React.Children.map(children, (Kid, i) => {
+	// 		console.log("clone", Kid);
+	// 		return React.cloneElement(Kid, {setShow});
+	// 	});
+	// }
+	return <Card body className={false&&'d-none'}><CardTitle><h4>{title}</h4></CardTitle>{children}</Card>;
+};
 
 export default SubCard;
  
