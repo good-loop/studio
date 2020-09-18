e1e71f51474f0bdbd914b324315d468d316312fd
Daniel Winterstein <daniel@good-loop.com>
2020-09-18 12:04:50 +0100

    MDText html and security

--- src/js/components/SubCard.jsx
+++ src/js/components/SubCard.jsx
@@ -1,11 +1,12 @@
 
 import React, { useState } from 'react';
 import {Card, CardTitle } from 'reactstrap';
+import { space } from '../base/utils/miscutils';
 
 /**
  * A Card for use inside a larger Card
  */
-const SubCard = ({title,children}) => {
+const SubCard = ({title,children,keywords}) => {
 	// TODO hide if nothing is visible
 	// let [show, setShow] = useState(false);
 	// // pass in setShow
@@ -17,6 +18,17 @@ const SubCard = ({title,children}) => {
 	// 		return React.cloneElement(Kid, {setShow});
 	// 	});
 	// }
+
+	// 	array of elements (or just one)? screen nulls
+	if (children.filter) children = children.filter(x => !! x);
+	
+	children = React.Children.map(children, (Kid, i) => {
+		console.log("clone", Kid);
+		let kws = Kid.props && Kid.props.keywords;
+		let kws2 = space(kws, title, keywords);
+		return React.cloneElement(Kid, {keywords:kws2});
+	});
+
 	return <Card body><CardTitle><h4>{title}</h4></CardTitle>{children}</Card>;
 };
 
