a75d6ba38305a5ed34338ea97d4c98c640d60cca
Daniel Winterstein <daniel@good-loop.com>
2020-12-08 12:50:23 +0000

    DragDropTest

---
+++ src/js/components/DragDropWidgets.jsx
@@ -0,0 +1,37 @@
+
+// import { Draggable, DropZone } from 'react-drag-or-touch-drop';
+import React, { useState } from 'react';
+// import { Draggable, DropZone } from '../base/components/DragDrop';
+import { Draggable, DropZone } from 'react-drag-or-touch-drop';
+import { Badge } from 'reactstrap';
+import DataStore from '../base/plumbing/DataStore';
+import SubCard from './SubCard';
+import WidgetExample from './WidgetExample';
+
+
+const DragDropWidgets = () => {
+	
+	let [list] = useState([]);
+	let textForDraggableId = {d1:"Drag me", d2:"Yeh!"};
+
+	return (<SubCard title="Drag and Drop">
+
+		<WidgetExample name="Drag and Drop - simple drop-in list">
+			
+			<Draggable id='d1'><div className='badge badge-success w-25'>{textForDraggableId.d1}</div></Draggable>
+			<Draggable id='d2'><div className='badge badge-info w-25'>{textForDraggableId.d2}</div></Draggable>
+			
+			<DropZone id='dropzone1' 
+				onDrop={(e, dropInfo) => list.push(textForDraggableId[dropInfo.draggable]) && DataStore.update() && console.log("You dropped this", dropInfo)}
+			>
+				<p>Drop here</p>
+				<ol>
+					{list.map((item,i) => <li key={i}>{item}</li>)}
+				</ol>
+			</DropZone>
+		</WidgetExample>
+
+	</SubCard>);
+};
+
+export default DragDropWidgets;
