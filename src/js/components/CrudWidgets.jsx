c869b8bda702bfc50f239977e0e7a4406670fd33
Daniel Winterstein <daniel@good-loop.com>
2021-02-18 19:17:57 +0000

    PropControlDataItem normalise

--- src/js/components/CrudWidgets.jsx
+++ src/js/components/CrudWidgets.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 
 import { Input, FormGroup, Label, Form, Card, CardTitle } from 'reactstrap';
-
+import {normaliseSogiveId} from '../base/plumbing/ServerIOBase';
 import PropControl, { FormControl } from '../base/components/PropControl';
 import DataStore, { getValue } from '../base/plumbing/DataStore';
 import Wizard, { WizardStage } from '../base/components/WizardProgressWidget';
@@ -31,11 +31,20 @@ const CrudWidgets = () => {
 	</Card>);
 };
 
+DataStore.setValue(['misc','crudtest','selectngoold'], "Foo Bar");
+
 const SelectDataItemCard = () => {
+	let norm = v => {
+		return normaliseSogiveId(v);
+	};
 	return (<SubCard title="CRUD select DataItem">
 		<WidgetExample name="Select NGO" keywords={baseKeywords}>
-			<PropControl prop='selectngo' path={['misc','crudtest']} type='DataItem' itemType='NGO' status={C.KStatus.PUBLISHED} detail='lite' sort='id' />
+			<PropControl modelValueFromInput={norm} prop='selectngo' path={['misc','crudtest']} type='DataItem' itemType='NGO' status={C.KStatus.PUBLISHED} detail='lite' sort='id' />
 			<pre>{JSON.stringify(DataStore.getValue('misc','crudtest','selectngo'))}</pre>
+		</WidgetExample>
+		<WidgetExample name="NGO - preserve old bad format value" keywords={baseKeywords}>
+			<PropControl modelValueFromInput={norm} prop='selectngoold' path={['misc','crudtest']} type='DataItem' itemType='NGO' status={C.KStatus.PUBLISHED} detail='lite' sort='id' />
+			<pre>{JSON.stringify(DataStore.getValue('misc','crudtest','selectngoold'))}</pre>
 		</WidgetExample>
 		<WidgetExample name="Select Test Agency canCreate" keywords={baseKeywords}>
 			<PropControl canCreate prop='selectagency' path={['misc','crudtest']} type='DataItem' itemType='Agency' status={KStatus.PUB_OR_DRAFT} />
