e6a8c3649fbc25873ffca2e8c77bd5bac7bb8890
Daniel Winterstein <daniel@good-loop.com>
2021-09-07 18:33:19 +0100

    slide-out

--- src/js/components/PageLayoutsPage.jsx
+++ src/js/components/PageLayoutsPage.jsx
@@ -4,23 +4,23 @@
  */
 import React from 'react';
 import { Alert, Card, CardTitle, Container } from 'reactstrap';
-import Editor3ColLayout from '../base/components/Editor3ColLayout';
+import Editor3ColLayout, { LeftSidebar, MainPane, RightSidebar } from '../base/components/Editor3ColLayout';
 import PropControl from '../base/components/PropControl';
 import DataStore from '../base/plumbing/DataStore';
 
 const PageLayoutsPage = () => {
 	let layout = window.location.hash.split(/[/?]/)[1];
-	if ( ! layout) {
+	if (!layout) {
 		return (<ul>
 			<li><a href='#pagelayouts/editor3col'>Editor3Col</a></li>
 			<li><a href='#pagelayouts/fullwidth'>Full Width</a></li>
 		</ul>);
 	}
-	switch(layout) {
-	case "editor3col":
-		return <Editor3ColEg />;
-	case "fullwidth":
-		return <FullWidth />;
+	switch (layout) {
+		case "editor3col":
+			return <Editor3ColEg />;
+		case "fullwidth":
+			return <FullWidth />;
 	}
 	return <Alert>Huh? <code>{layout}</code></Alert>
 };
@@ -32,11 +32,11 @@ const FullWidth = () => {
 	</div>);
 };
 
-const LoremIpsum = ({words=100}) => {
+const LoremIpsum = ({ words = 100 }) => {
 	let s = '';
 	const il = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".split(" ");
-	for(let i=0; i<words; i++) {
-		s += il[i % il.length]+i+" ";
+	for (let i = 0; i < words; i++) {
+		s += il[i % il.length] + i + " ";
 	}
 	return <p>{s}</p>;
 };
@@ -45,27 +45,27 @@ const Editor3ColEg = () => {
 	// HACK
 	window.fullWidthPage = true;
 	const is2 = DataStore.getUrlValue('is2');
-	
-	let $main = (<div style={{background:'rgba(128,128,255,128)', height:'150vh'}}>
+
+	let $main = (<div style={{ background: 'rgba(128,128,255,128)', height: '150vh' }}>
 		<h1>Editor3ColLayout - Main Content</h1>
 		Space for lots of stuff
 		<PropControl size='lg' prop='is2' type='checkbox' label='2 Columns Only?' />
 		<LoremIpsum words={50} />
 		<LoremIpsum words={50} />
 	</div>);
-	let $right = <div style={{background:'rgba(255,128,128,128)'}}>Extra panel - which does not scroll <LoremIpsum words={100} /></div>;
+	let $right = <div style={{ background: 'rgba(255,128,128,128)' }}>Extra panel - which does not scroll <LoremIpsum words={100} /></div>;
 
 	if (is2) {
 		return (<Editor3ColLayout>
-			{$main}
-			{$right}
-		</Editor3ColLayout>);	
+			<MainPane>{$main}</MainPane>
+			<RightSidebar>{$right}</RightSidebar>
+		</Editor3ColLayout>);
 	}
 
 	return (<Editor3ColLayout>
-		<div style={{background:'rgba(128,255,128,128)'}}>Sub page navigation</div>
-		{$main}
-		{$right}
+		<LeftSidebar><div style={{ background: 'rgba(128,255,128,128)' }}>Sub page navigation Slide<ol><li>Thing One</li><li>Thing Two</li></ol></div></LeftSidebar>
+		<MainPane>{$main}</MainPane>
+		<RightSidebar>{$right}</RightSidebar>
 	</Editor3ColLayout>);
 };
 
