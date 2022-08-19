1f1522826fb2062ebccac6240da81efe8b9ecad6
Lewis <lewis.frampton@gmail.com>
2022-08-19 12:44:58 +0100

    added testing widgets for modals & styled modals

--- src/js/components/LayoutWidgets.jsx
+++ src/js/components/LayoutWidgets.jsx
@@ -33,7 +33,7 @@ const LayoutWidgets = () => {
 						<PropControl prop='compact-1' label='My first compact label' value='Some text' />
 						<PropControl prop='compact-2' label='Another label with some help' help='How can we help you?' />
 						<PropControl prop='compact-3' label='Checkbox 1' type='checkbox' warning='Oh dear, blah blah' />
-						<PropControl prop='compact-4' label='Some free text' type='textarea' />
+						<PropControl prop='compact-4' label='Some free text' type='text' modal/>
 						<Button>A Button</Button>
 					</div>
 				</Col>
@@ -42,7 +42,7 @@ const LayoutWidgets = () => {
 						<PropControl prop='compact-1' label='My first normal label' />
 						<PropControl prop='compact-2' label='Another label with some help' help='How can we help you?' />
 						<PropControl prop='compact-3' label='Checkbox 1' type='checkbox' warning='Oh dear, blah blah' />
-						<PropControl prop='compact-4' label='Some free text' type='textarea' />
+						<PropControl prop='compact-LEWIS' fast label='ModalTestingElement' modal styled="js" type='textarea' />
 						<Button>Finally, a button</Button>
 					</div>
 				</Col>
@@ -54,9 +54,34 @@ const LayoutWidgets = () => {
 				<PropControl prop='compact-1' label='My first inline label' value='Some text' />
 				<PropControl prop='compact-2' label='Another label with some help' help='How can we help you?' />
 				<PropControl prop='compact-3' label='Checkbox 1' type='checkbox' warning='Oh dear, blah blah' />
-				<PropControl prop='compact-4' label='Some free text' type='textarea' />
+				<PropControl prop='compact-4' label='Some free text' type='textarea'/>
 				<Button>A Button</Button>
 			</Form>
+		</WidgetExample>
+		<WidgetExample name='Modal Layout' keywords='' >
+			<Form inline>
+				<PropControl prop='compact-1' label='My first inline label' value='Some text' />
+				<PropControl prop='compact-2' label='Another label with some help' help='How can we help you?' />
+				<PropControl prop='compact-3' label='Checkbox 1' type='checkbox' warning='Oh dear, blah blah' />
+				<PropControl prop='compact-4' label='Some free text' type='textarea'/>
+				<Button>A Button</Button>
+			</Form>
+		</WidgetExample>
+		<WidgetExample name='Modal textareas/text' keywords='' >
+			<Row>
+				<Col>
+					<div>
+					<PropControl fast prop='compact-5' label='Modal textarea' value='Some text' type="textarea" modal/>
+					<PropControl fast prop='compact-6' label='Modal text'  value='Some text' type="text" modal/>
+					</div>
+				</Col>
+				<Col>
+					<div>
+						<PropControl fast prop='compact-7' label='Modal Styled CSS' value="p {colour : red}" type="textarea" modal styled="css"/>
+						<PropControl fast prop='compact-8' label='Modal Styled JS' value="let x = true" type="textarea" modal styled="js"/>
+					</div>
+				</Col>
+			</Row>
 		</WidgetExample>
 	</SubCard>);
 };
