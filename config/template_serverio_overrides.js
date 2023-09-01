5844dc5cd03cc166479001cd23084f799b6c2362
Roscoe <roscoe@good-loop.com>
2023-09-01 12:34:17 +0100

    See matching wwappbase.js branch - update webpack and host configs to new format

--- config/template_serverio_overrides.js
+++ config/template_serverio_overrides.js
@@ -2,15 +2,16 @@
 // You don't have to commit it, but it won't affect any other machines if you do.
 // The setup below is only an example - you can mix and match servers and hardcode whatever you want.
 
-// Change to "local", "test" or "" to switch all endpoints together
-const cluster = 'test';
-const protocol = (cluster === 'local') ? 'http' : 'https';
+// Change index to switch all endpoints together
+const cluster = ['', 'stage', 'test', 'local'][3];
 
-module.exports = {
-	ServerIOOverrides: {
-    APIBASE: `${protocol}://${cluster}portal.good-loop.com`,
-    DATALOG_ENDPOINT: `${protocol}://${cluster}lg.good-loop.com/data`,
-    PROFILER_ENDPOINT: `${protocol}://${cluster}profiler.good-loop.com`,
-		MEDIA_ENDPOINT: `${protocol}://${cluster}uploads.good-loop.com`,
-	}
+// Change to "http" if you don't have SSL set up locally
+const PROTOCOL_LOCAL = 'https';
+const protocol = (cluster === 'local') ? PROTOCOL_LOCAL : 'https';
+
+export const ServerIOOverrides = {
+	APIBASE: `${protocol}://${cluster}portal.good-loop.com`,
+	DATALOG_ENDPOINT: `${protocol}://${cluster}lg.good-loop.com/data`,
+	PROFILER_ENDPOINT: `${protocol}://${cluster}profiler.good-loop.com`,
+	MEDIA_ENDPOINT: `${protocol}://${cluster}uploads.good-loop.com`,
 };
