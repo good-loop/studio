0ab56a1e47d4fb0b36f279c5655b28bde5609b0d
Daniel Winterstein <daniel@good-loop.com>
2023-11-14 18:43:19 +0000

    playwright

--- test/js/hello-world.test.js
+++ test/js/hello-world.test.js
@@ -1,14 +1,14 @@
 
-// Status: This runs ...but I cannot get something DataStore.js with dependencies to run
+// // Status: This runs ...but I cannot get something DataStore.js with dependencies to run
 
-let test = require('ava');
-// import test from 'ava';
+// let test = require('ava');
+// // import test from 'ava';
 
-test('smoketest ava - Hello World', t => {
-	t.pass();
-});
+// test('smoketest ava - Hello World', t => {
+// 	t.pass();
+// });
 
-test('smoketest ava - await Hello World', async t => {
-	const bar = Promise.resolve('Hello World');
-	t.is(await bar, 'Hello World');
-});
+// test('smoketest ava - await Hello World', async t => {
+// 	const bar = Promise.resolve('Hello World');
+// 	t.is(await bar, 'Hello World');
+// });
